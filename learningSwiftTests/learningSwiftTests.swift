47313639e698a8b038d7c1553cc1934b75068ef6
egeyigit <92596279+egeyigit@users.noreply.github.com>
2025-02-23 22:47:16 +0300

    Initial Commit

---
+++ learningSwiftTests/learningSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  learningSwiftTests.swift
+//  learningSwiftTests
+//
+//  Created by Ege Yiğit Güven on 23.02.2025.
+//
+
+import Testing
+@testable import learningSwift
+
+struct learningSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
